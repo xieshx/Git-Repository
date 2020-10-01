a5658696cd603368592a0f18c928a6b1e5a12461
xieshixiao <13080867160@163.com>
2020-10-01 19:55:31 +0800

    主分支修改了maven仓库

---
+++ supermarket/src/main/java/com/neu/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.neu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+
+}
