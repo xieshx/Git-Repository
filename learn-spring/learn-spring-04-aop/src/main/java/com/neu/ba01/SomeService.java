67bebcee873779a6b7014d173d7d4fe3898611f4
xieshixiao <13080867160@163.com>
2020-10-09 19:54:41 +0800

    清空主分支

--- learn-spring/learn-spring-04-aop/src/main/java/com/neu/ba01/SomeService.java
+++
@@ -1,7 +0,0 @@
-package com.neu.ba01;
-
-public interface SomeService {
-    void doSomeBefore(String name, Integer age);
-    String doSomeAfterReturning(String name, Integer age);
-    String doSomeAround(String name, Integer age);
-}
--- learn-spring/learn-spring-07-goods-aspectj/src/main/java/com/neu/dao/SaleDao.java
+++
@@ -1,7 +0,0 @@
-package com.neu.dao;
-
-import com.neu.domain.Sale;
-
-public interface SaleDao {
-    int insertSale(Sale sale);
-}
