5b569a92767648c1a9b37e88e8b11374646b409c
xieshixiao <13080867160@163.com>
2020-11-01 09:10:00 +0800

    清空Python

--- versionid/views/menu/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from versionid.views.menu import views
-
-urlpatterns = [
-    # 进入ID管理界面
-    path('enter/', views.enter, name='enter'),
-]
--- versionid/views/versionid/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from versionid.views.versionid import version_register
-
-urlpatterns = [
-    path('register/', version_register.main, name='version_register'),
-]
