c97c9386bf59f3b013abb4204135af7045b309f2
xieshixiao <13080867160@163.com>
2021-03-10 21:10:39 +0800

    正在进行右侧导航栏

--- ms_front/src/router/index.js
+++ ms_front/src/router/index.js
@@ -4,20 +4,39 @@ import VueRouter from "vue-router"//引入 Vue 路由
 Vue.use(VueRouter);//安装VueRouter插件
 
 const constantRouterMap = [
+    {
+        path: '/',
+        component: () => import('@/views/layout'),
+        redirect: '/home',
+        // 添加children属性，子组件会被渲染在父组件的<router-view/>中
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('@/views/home/index')
+            },
+            {
+                path: '/goods',
+                name: 'goods',
+                component: () => import('@/views/goods/index')
+            },
+            {
+                path: '/admin',
+                name: 'admin',
+                component: () => import('@/views/admin/index')
+            }
+        ]
+    },
     {path: '/login', component: () => import('@/views/login')},
     {path: '/error', component: () => import('@/views/error')},
-    {path: '/home', component: () => import('@/views/home')}
 ];
 
 // 1.URL上的#号的特性（即改变 URL 却不请求后端），就可以在前端实现页面的整体变化，而不用每次都去请求后端，
 // 这种模式被称为hash模式
 // 2.另一种常用的模式是history模式，这种模式的原理是先把页面的状态保存到一个对象（state）里
 // 当页面的 URL 变化时找到对应的对象，从而还原这个页面，使用了这种模式，就可以摆脱#号实现前端路由。
-const router = new VueRouter({
+export default new VueRouter({
     //把 Vue 中配置的路由从默认的 hash 模式切换为 history 模式
     mode: 'history',
     routes: constantRouterMap
 });
-
-
-export default router
--- ms_front/src/views/layout/index.vue
+++ ms_front/src/views/layout/index.vue
@@ -1,16 +1,113 @@
 <template>
-    <div>
-        <div class="sidebar-container"></div>
-        <div class="main-container"></div>
-    </div>
+    <el-container>
+        <el-aside width="200px">
+            <el-menu router
+                     @open="handleOpen"
+                     @close="handleClose"
+                     background-color="#304156"
+                     text-color="#bfcbd9"
+                     active-text-color="#409eff">
+                <el-menu-item index="/home">
+                    <i class="el-icon-s-home"></i>
+                    <template #title>首页</template>
+                </el-menu-item>
+                <el-menu-item index="/goods">
+                    <i class="el-icon-shopping-cart-2"></i>
+                    <template #title>商品列表</template>
+                </el-menu-item>
+                <el-submenu index="/admin">
+                    <i class="el-icon-s-custom"></i>
+                    <template #title>管理员</template>
+                    <el-menu-item-group>
+                        <el-menu-item>用户列表</el-menu-item>
+                        <el-menu-item>角色列表</el-menu-item>
+                    </el-menu-item-group>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-header height="40px">
+                <!--el-row的高度是被子元素撑起来的（auto），需要设置占据el-header100%高度才能用align="middle"看出垂直居中的效果-->
+                <el-row type="flex" justify="space-between" align="middle">
+                    <el-col>
+                        <el-breadcrumb separator-class="el-icon-arrow-right">
+                            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+                            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+                            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+                            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+                        </el-breadcrumb>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-dropdown trigger="click">
+                            <span class="el-dropdown-link">
+                                {{username}}
+                                <i class="el-icon-arrow-down"></i>
+                            </span>
+                            <template #dropdown>
+                                <el-dropdown-menu slot="dropdown">
+                                    <!--给Vue组件绑定事件,需要在事件后面加上.native,有些组件(el-button..),不用加native是因为底层源码已经实现了native-->
+                                    <el-dropdown-item icon="el-icon-switch-button" @click.native="logout">退出
+                                    </el-dropdown-item>
+                                </el-dropdown-menu>
+                            </template>
+                        </el-dropdown>
+                    </el-col>
+                </el-row>
+            </el-header>
+            <el-divider></el-divider>
+            <el-main>
+                <router-view/>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
+    import {getCookie} from "@/utils/support";
+
     export default {
-        name: "index"
+        name: "layout",
+        data() {
+            return {
+                isCollapse: false,
+                username: getCookie('username')
+            }
+        },
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            logout() {
+                this.$store.dispatch('LogOut').then(() => {
+                    // js刷新，相当于在浏览器上按下刷新按钮
+                    location.reload()
+                })
+            }
+        }
     }
 </script>
 
 <style scoped>
+    .el-divider {
+        margin-top: 1px;
+        margin-bottom: 1px;
+    }
 
+    .el-dropdown-link,
+    .el-icon-switch-button {
+        cursor: pointer;
+        font-size: 15px;
+    }
+
+    .el-aside {
+        background-color: #304156;
+        height: 100vh;
+    }
+
+    .el-row {
+        height: 100%;
+    }
 </style>
